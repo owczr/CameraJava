a112ea5d7cc90e8be7d60b7e464caad227124a9e
Jakub Owczarek <jakub.owczarek@codete.com>
2022-12-15 08:49:04 +0100

    Remove Frames

--- cam/Cam.java
+++ cam/Cam.java
@@ -1,7 +1,8 @@
 package cam;
 
 import java.nio.ByteBuffer;
- 
+import java.util.Arrays;
+
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -13,88 +14,60 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.util.Duration;
 
-public class Cam extends Application 
- {
-  private static final int FRAME_WIDTH  = 640;
-  private static final int FRAME_HEIGHT = 480;  
-
-
+public class Cam extends Application {
+    private static final int FRAME_WIDTH = 640;
+    private static final int FRAME_HEIGHT = 480;
     GraphicsContext gc;
     Canvas canvas;
     byte[] buffer;
-    PixelWriter pixelWriter; 
+    PixelWriter pixelWriter;
     PixelFormat<ByteBuffer> pixelFormat;
 
-    Frames frames;
-         
-    public static void main(String[] args) 
-     {
-      launch(args);
-     }
+    //Frames frames;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
-    public void start(Stage primaryStage) 
-     {
-      int result;
-      
-      Timeline timeline;
-      
-      frames = new Frames();
-      
-      result = frames.open_shm("/frames");
-      
-      primaryStage.setTitle("Camera");
-      Scene scene;
-      
-      Group root = new Group();
-      canvas     = new Canvas(650, 490);
-      gc         = canvas.getGraphicsContext2D();
+    public void start(Stage primaryStage) {
+        int result;
 
-			    
- 
-      timeline = new Timeline(new KeyFrame(Duration.millis(130), e->disp_frame()));
+        Timeline timeline;
 
-      timeline.setCycleCount(Timeline.INDEFINITE);
+        //frames = new Frames();
 
-      timeline.play();
- 
-      root.getChildren().add(canvas);
-      scene = new Scene(root);
-      primaryStage.setScene(scene);
-      primaryStage.show();
+        //result = frames.open_shm("/frames");
+
+        primaryStage.setTitle("Camera");
+        Scene scene;
+
+        Group root = new Group();
+        canvas = new Canvas(650, 490);
+        gc = canvas.getGraphicsContext2D();
+
+
+        timeline = new Timeline(new KeyFrame(Duration.millis(130), e -> disp_frame()));
+
+        timeline.setCycleCount(Timeline.INDEFINITE);
+
+        timeline.play();
+
+        root.getChildren().add(canvas);
+        scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
-    
-    private void disp_frame()
-     {
-      
-      pixelWriter = gc.getPixelWriter();
-      pixelFormat = PixelFormat.getByteRgbInstance();
-            
 
-      buffer = frames.get_frame();
-      pixelWriter.setPixels(5, 5, FRAME_WIDTH, FRAME_HEIGHT, pixelFormat, buffer, 0, FRAME_WIDTH*3);
-      
-     }
-    
- }
+    private void disp_frame() {
+        pixelWriter = gc.getPixelWriter();
+        pixelFormat = PixelFormat.getByteRgbInstance();
 
+        //buffer = frames.get_frame();
 
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        byte[] bytes = new byte[3 * 480 * 640];
+        Arrays.fill( bytes, (byte) 0xF );
+        buffer = bytes;
+        pixelWriter.setPixels(5, 5, FRAME_WIDTH, FRAME_HEIGHT, pixelFormat, buffer, 0, FRAME_WIDTH * 3);
+    }
+}
